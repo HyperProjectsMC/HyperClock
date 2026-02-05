c4b6ae628194ac9f5fd7ab14de7bdd7c96603c9b
Timor2011pro <timorrigter@gmail.com>
2026-02-05 20:54:46 +0100

    Initial commit

---
+++ src/main/java/org/hyperprojects/hyperclock/HyperClock.java
@@ -0,0 +1,17 @@
+package org.hyperprojects.hyperclock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HyperClock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
